56ddb1b86a0c90a41834750c9601e26e783cb97a
David Flynn <daflynn@csumb.edu>
2018-09-14 16:47:37 +0000

    homework 1

---
+++ labs/lab2/rand copy.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta char="utf-8" />
+    </head>
+    
+    <body>
+        
+        
+        <?php
+        echo "<table border='1'><br />";
+        
+        $sum;
+        $avg;
+        
+        for ($row = 0; $row < 2; $row ++) {
+            echo "<tr>";
+    
+            for ($col = 0; $col < 9; $col ++) {
+            if($row == 1)
+            {
+            $number = rand(1,10);
+            $sum += $number;
+            $iseven;
+            if($number % 2 == 0)
+            {
+                $iseven = "Even";
+            }
+            else
+            {
+                $iseven = "Odd";
+            }
+            
+            echo "<td>", $number, " is ",$iseven,"</td>";
+            }
+            
+            }
+    
+            echo "</tr>";
+        }
+
+echo "</table>";
+
+echo "Total: ", $sum;
+
+echo "<br/>", "Average: ", $sum / 9;
+?>
+        
+        
+        
+        
+    </body>
+    
+    
+    
+</html>
