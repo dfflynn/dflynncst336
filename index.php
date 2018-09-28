7e664ce32fcf178aa246361e7333206bea69780b
David Flynn <daflynn@csumb.edu>
2018-09-28 17:47:38 +0000

    updated links

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
     <ul>
         <li><a href = "./homework/1">Homework 1</a></li>
+        <li><a href = "./homework/2">Homework 2</a></li>
         <li><a href = "./labs/lab1/portfolio">Lab 1</a></li>
         <li><a href = "./labs/lab2/777">Lab 2</a></li>
         <li><a href = "./practice/practice1/">Practice Lab 1</a></li>
